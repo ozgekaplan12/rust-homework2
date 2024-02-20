73a9e90a1e15700bfd1e1525b313cbed926814fe
Özge <92800183+ozgekaplan12@users.noreply.github.com>
2024-02-20 23:52:08 +0300

    lib.rs

--- src/library0_backend/src/lib.rs
+++ src/library0_backend/src/lib.rs
@@ -15,10 +15,39 @@ struct Magazine {
     topic: String,
 }
 
-fn library_publications(publications:Vec<Publication>) {
+fn print_publications(publications: Vec<Publication>) {
     for publication in publications {
         match publication {
-            
+            Publication::Book(ref book) => {
+                println!(
+                    "Book: {} author: {}, {} pages",
+                    book.title, book.author, book.page_count
+                );
+            }
+            Publication::Magazine(ref magazine) => {
+                println!(
+                    "Magazine: {} - Issue: {}, Topic: {}",
+                    magazine.title, magazine.issue, magazine.topic
+                );
+            }
         }
     }
+}
+
+fn main() {
+    let book1 = Book {
+        title: "The Rust Programming Language".to_string(),
+        author: "K. Rustacean".to_string(),
+        page_count: 320,
+    };
+
+    let magazine1 = Magazine {
+        title: "Rust World".to_string(),
+        issue: 10,
+        topic: "Systems Programming".to_string(),
+    };
+
+    let publications = vec![Publication::Book(book1), Publication::Magazine(magazine1)];
+
+    print_publications(publications);
 }
